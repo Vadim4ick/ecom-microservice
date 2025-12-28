2273bd74bed8f4d75867ce00c611b58702df03bc
Vadim <a-yva-2013@mail.ru>
2025-12-28 21:40:34 +0400

    ✨ (page.tsx, Categories.tsx, ProductCard.tsx, ProductLixt.tsx, products.const.tsx, product.type.ts): добавлены компоненты для отображения продуктов и категорий 📝 (products.const.tsx): добавлены данные о продуктах и категориях Созданы новые компоненты для отображения списка продуктов и категорий на главной странице. Это улучшает пользовательский интерфейс, позволяя пользователям легко просматривать и выбирать продукты по категориям. Также добавлены типы для продуктов, что улучшает типизацию и поддержку в коде.

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,11 +1,20 @@
+import { ProductList } from "@/components/ProductLixt";
 import Image from "next/image";
 
-const Homepage = () => {
+const Homepage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ category: string }>;
+}) => {
+  const category = (await searchParams).category;
+
   return (
     <div className="">
       <div className="relative aspect-[3/1] mb-12">
         <Image src={"/featured.png"} fill alt="featured" />
       </div>
+
+      <ProductList params="homepage" category={category} />
     </div>
   );
 };
