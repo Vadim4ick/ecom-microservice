05dd02d10ad95a8170a396c3f96303befbb2c2c7
Vadim <a-yva-2013@mail.ru>
2026-01-05 00:05:34 +0400

    ✨ (order-service): добавлен новый сервис на основе Fastify 🔧 (order-service): обновлены зависимости и скрипты в package.json ♻️ (order-service): упрощен tsconfig.json для order-service и product-service
    Создан новый сервис order-service, использующий Fastify для обработки запросов. Обновлены зависимости, включая добавление Fastify и TypeScript, а также добавлен новый скрипт для разработки. Упрощены конфигурации tsconfig.json для order-service и product-service, удалив ненужные параметры, что улучшает читаемость и поддержку кода.

---
+++ apps/order-service/src/index.ts
@@ -0,0 +1,20 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({
+      port: 8001,
+    });
+
+    fastify.log.info(`server listening on 8001`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
